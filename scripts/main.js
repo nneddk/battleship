f19e3ff567f7bf7f74085c887195591e823b8221
nneddk <meljandre.mc@gmail.com>
2022-09-24 19:51:17 +0800

    placement edit added

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 const newGame = () =>{
     const newGame = document.createElement('div');
     newGame.classList.add('new-game');
-    let currBlock = 0;
+    
     //1 for vertical, 0 for horizontal
     let orientation = 1;
     // d = destroyer, s = submarine, cruiser = c, b = battleship, C = carrier
@@ -11,34 +11,20 @@ const newGame = () =>{
                       [4,'b', 4, 'purple'], 
                       [5,'C', 5, 'orange']
                     ];
+    let currP = 0;
+    let currBlock = gamePiece[currP];
 
     const blockHolder = () =>{
         const blockHolder = document.createElement('div');
         blockHolder.classList.add('block-holder');
-        
-        const blockHolderHeader = document.createElement('div');
-        blockHolderHeader.classList.add('block-holder-header');
-        blockHolderHeader.textContent = 'Blocks:';
 
-        const blockHolderHolder = document.createElement('div');
-        blockHolderHolder.classList.add('block-holder-holder');
-        
-        for (let i = 0; i <5; i++){
-            const blockDiv = document.createElement('button');
-            blockDiv.classList.add(gamePiece[i][1]);
-            blockDiv.classList.add('block');
-            blockDiv.textContent = gamePiece[i][1];
-
-            blockDiv.onclick = () =>{
-                currBlock = gamePiece[i];
-                console.log(currBlock[2]);
-            }
-
-            blockHolderHolder.appendChild(blockDiv);
+        const rotateBtn = document.createElement('button');
+        rotateBtn.classList.add('rotate-button');
+        rotateBtn.textContent = 'rotate';
+        rotateBtn.onclick = ()=>{
+            orientation = !orientation;
         }
-        
-        blockHolder.appendChild(blockHolderHeader);
-        blockHolder.appendChild(blockHolderHolder);
+        blockHolder.appendChild(rotateBtn);
         return blockHolder;
     }
     
@@ -60,7 +46,7 @@ const newGame = () =>{
             for(let x = 0; x < 7; x++){
                 const gamePixel = document.createElement('div');
                 gamePixel.classList.add('game-pixel')
-                gameState[y][x] = [0, gamePixel];
+                gameState[y][x] = [0, gamePixel, 0, 0];
     
                 gamePixel.onmouseover =()=>{
                     pixelHighlight(y, x, true, currBlock);
@@ -71,7 +57,6 @@ const newGame = () =>{
 
                 gamePixel.onclick =()=>{
                     pixelClick(y, x, currBlock);
-                    console.log(gameState);
                     
                 }
                 
@@ -90,7 +75,7 @@ const newGame = () =>{
             let pieceAvailable = true;
     
             for(let i = 0; i<piece; i++){
-                if (gameState[y + i]==null && orientation){
+                if (gameState[y + i] == null && orientation){
                     pieceAvailable = false;
                 }else if (gameState[y + (orientation?i:0)] [x + (orientation?0:i)] == null){
                     pieceAvailable = false;
@@ -115,11 +100,32 @@ const newGame = () =>{
             }
 
         }
+        let editOn = false;
         function pixelClick(y, x, cB){
             let pieceAvailable = true;
             let color = cB[3]
             let piece = cB[2];
-    
+            let token = cB[0];
+
+            if(gameState[y][x][0] != 0 && !editOn){
+                editOn = true;
+                let tokenChecker = gameState[y][x][0];
+                for(let i = 0; i < 7; i++){
+                    for(let j = 0; j < 7; j++){
+                        if(gameState[i][j][0] == tokenChecker){
+                            currBlock = gamePiece[tokenChecker - 1];
+                            gameState[i][j][0] = 0;
+                            gameState[i][j][2] = 0;
+                            gameState[i][j][1].classList.remove(color);
+                            gameState[i][j][1].style.backgroundColor = '';
+                            orientation = gameState[i][j][3];
+                        }
+                    }
+                }
+                pieceAvailable = false;
+
+            }
+
             for(let i = 0; i<piece; i++){
                 if (gameState[y + i]==null && orientation){
                     pieceAvailable = false;
@@ -130,13 +136,28 @@ const newGame = () =>{
                 }
                 
             }
-            console.log(pieceAvailable);
             if(pieceAvailable){
                 for(let i = 0; i< piece; i++){
-                    gameState[y + (orientation?i:0)] [x + (orientation?0:i)][1].style.backgroundColor = color;
-                    gameState[y + (orientation?i:0)] [x + (orientation?0:i)][0] = piece;
+
+                    gameState[y + (orientation?i:0)] [x + (orientation?0:i)][1].classList.add(color);
+                    gameState[y + (orientation?i:0)] [x + (orientation?0:i)][0] = token;
+                    gameState[y + (orientation?i:0)] [x + (orientation?0:i)][2] = piece;
+                    gameState[y + (orientation?i:0)] [x + (orientation?0:i)][3] = orientation;
                 }
+                if (currP <=3){
+                    if(!editOn){
+                        currBlock = gamePiece[++currP];
+                    }else if(editOn){
+                        currBlock = gamePiece[currP];
+                        
+                    }
+                }else{
+                    currBlock = 0;
+                }
+                editOn = false;
             }
+            
+            
         }
     
         return newGameboard;
